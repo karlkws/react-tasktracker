30089fbe576395754f602fbdddeec01ae0dc0f9a
Karl <karlkws95@gmail.com>
2021-11-10 20:39:16 +0800

    update port

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 import Home from "./components/Home";
 
+const port = 5001
+
 /* Function based component */
 // Arrow function
 const App = () => {
@@ -33,14 +35,14 @@ const App = () => {
 
   // Fetch all Tasks
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch(`http://localhost:${port}/tasks`);
     const data = await res.json();
     return data;
   };
 
   // Fetch one Task for updating Reminder to server
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(`http://localhost:${port}/tasks/${id}`);
     const data = await res.json();
     return data;
   };
@@ -49,7 +51,7 @@ const App = () => {
   const deleteTask = async (id) => {
     // console.log('Delete', id)
 
-    await fetch(`http://localhost:5000/tasks/${id}`, {
+    await fetch(`http://localhost:${port}/tasks/${id}`, {
       method: "DELETE",
     });
 
@@ -62,7 +64,7 @@ const App = () => {
     const taskToToggle = await fetchTask(id);
     const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder } // update task 'reminder' property to reverse boolean using ... operator
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    const res = await fetch(`http://localhost:${port}/tasks/${id}`, {
       method: "PUT", 
       headers: {
         "Content-type": "application/json",
@@ -81,7 +83,7 @@ const App = () => {
 
   // Add Task function
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
+    const res = await fetch(`http://localhost:${port}/tasks`, {
       method: "POST",
       headers: {
         "Content-type": "application/json",
